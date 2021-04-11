d451776868371e988f07a9b46386da1a0cb48e19
monkeydue <82245051+monkeydue@users.noreply.github.com>
2021-04-11 14:15:02 +0100

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    prefix: "!",
+    token: "TOKEN",
+    channel: "830043801895567400",
+    ownerID: "830036326652706816",
+    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+    channel_id: "YOUTUBE CHANNEL ID",
+    watchInterval: 30000
+}
